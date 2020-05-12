59120fef2cadc80df002ba82fcb6ef362ab0af46
MaheshtheDev <mahesh.svmr@gmail.com>
2020-05-12 10:52:43 +0530

    Updated Dev info

--- main.py
+++ main.py
@@ -30,12 +30,18 @@ heading_ll = "*Latest Articles on Data Science in Towards DataScience*"
 lat_links = ""
 for i in range(5,11):
     lat_links += "\n" + str(i-4) +". <a href=\""+links[i]+"\">"+art_name[i]+"</a>"
-
+sm_links = "<a href="https:\\\www.linkedin.com\in\maheshthedev\">" + "Linked In" +"</a>" + "\n" + "<a href="https:\\\github.com\maheshthedev">" + "Github" +"</a>"
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Welcome!')
-    bot.send_message(message.chat.id, 'Use /latest for Latest Articles')
-    bot.send_message(message.chat.id, 'Use /trend for Trending Articles')
+    bot.send_message(message.chat.id, 'Use /latest for Latest Articles \n Use /trend for Trending Articles')
+    bot.send_message(message.chat.id, 'Type Dev for Developer Information!')
+
+@bot.message_handler(commands=['Dev'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "I'm made by MaheshtheDev. He is passionate about Data Science")
+    bot.send_message(message.chat.id, "You can Connect with my Developer MaheshtheDev")
+    bot.send_message(message.chat.id, sm_links,parse_mode='HTML')
 
 @bot.message_handler(commands=['trend'])
 def send_message(message):
