a3d4e48d450156727db49bc6ba2911ad27835966
MaheshtheDev <mahesh.svmr@gmail.com>
2020-05-13 15:12:45 +0530

    Bug Fixes 1.0

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ bot_token = '1000110388:AAFdfrAD61GecD7sphhi2nvyGb_R_vu0xQc'
 bot = telebot.TeleBot(token=bot_token)
 server = Flask(__name__)
 
-page=requests.get("https://towardsdatascience.com/data-science/home")
-soup = BeautifulSoup(page.content,'html.parser')
+page1ds=requests.get("https://towardsdatascience.com/data-science/home")
+soup = BeautifulSoup(page1ds.content,'html.parser')
 t2 = soup.find_all(class_="u-contentSansBold u-lineHeightTightest u-xs-fontSize24 u-paddingBottom2 u-paddingTop5 u-fontSize32")
-art_name = []
+art_nameds = []
 for i in t2:
     j = list(i)
     j1 = list(j[0])
-    art_name.append(j1[0])
+    art_nameds.append(j1[0])
 l = []
 for i in soup.find_all('a',href=True):
     l.append(i['href'])
@@ -24,7 +24,7 @@ links = l[12:-6:4]
 heading = "*Top Stories in Data Science*"
 new_links = ""
 for i in range(5):
-    new_links += "\n" + str(i+1) +". <a href=\""+links[i]+"\">"+art_name[i]+"</a>" 
+    new_links += "\n" + str(i+1) +". <a href=\""+links[i]+"\">"+art_nameds[i]+"</a>" 
 
 heading_ll = "*Latest Articles on Data Science in Towards DataScience*"
 lat_links = ""
@@ -32,16 +32,16 @@ git_acc = "https://github.com/maheshthedev"
 linkin = "https://www.linkedin.com/in/maheshthedev/"
 for i in range(5,11):
     lat_links += "\n" + str(i-4) +". <a href=\""+links[i]+"\">"+art_name[i]+"</a>"
-sm_links = "<a href=\""+linkin+"\">"+"Linked In"+"</a>"
-sm_links +="·" + "<a href=\""+git_acc+"\">"+"Github"+"</a>"
+sm_links = "<a href=\""+linkin+"\">"+"LinkedIn"+"</a>"
+sm_links +=" · " + "<a href=\""+git_acc+"\">"+"GitHub"+"</a>"
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Welcome!')
-    bot.send_message(message.chat.id, 'Use /latest for Latest Articles \n Use /trend for Trending Articles \n Use /Dev for Developer Information')
+    bot.send_message(message.chat.id, 'Use /latest for Latest Articles \nUse /trend for Trending Articles \nUse /dev for Developer Information')
 
 @bot.message_handler(commands=['dev'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "I'm made by MaheshtheDev. He is passionate about Data Science \n You can Connect with my Developer:MaheshtheDev")
+    bot.send_message(message.chat.id, "I'm made by MaheshtheDev.\nHe is passionate about Data Science \nYou can Connect with my Developer:MaheshtheDev")
     bot.send_message(message.chat.id, sm_links,parse_mode='HTML')
 
 @bot.message_handler(commands=['trend'])
