a2eadae5cefe14a335ffe61eec970972d26a89d3
MaheshtheDev <mahesh.svmr@gmail.com>
2020-05-16 21:27:46 +0530

    inline test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import requests
 from flask import Flask, request
 from bs4 import BeautifulSoup
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 bot_token = '1000110388:AAFdfrAD61GecD7sphhi2nvyGb_R_vu0xQc'
 bot = telebot.TeleBot(token=bot_token)
 server = Flask(__name__)
@@ -37,8 +38,8 @@ git_acc = "https://github.com/maheshthedev"
 linkin = "https://www.linkedin.com/in/maheshthedev/"
 for i in range(5,11):
     lat_links += "\n" + str(i-4) +". <a href=\""+linksds[i]+"\">"+art_nameds[i]+"</a>"
-sm_links = "<a href=\""+linkin+"\">"+"LinkedIn"+"</a>"
-sm_links +=" · " + "<a href=\""+git_acc+"\">"+"GitHub"+"</a>"
+sm_links = "<a href=\""+git_acc+"\">"+"GitHub"+"</a>"
+sm_links +=" · " + "<a href=\""+linkin+"\">"+"LinkedIn"+"</a>"
 
 # pageml=requests.get("https://towardsdatascience.com/machine-learning/home")
 # soup = BeautifulSoup(pageml.content,'html.parser')
@@ -57,20 +58,43 @@ sm_links +=" · " + "<a href=\""+git_acc+"\">"+"GitHub"+"</a>"
 # for i in range(5):
 #     new_linksml += "\n" + str(i+1) +". <a href=\""+linksml[i]+"\">"+art_nameml[i]+"</a>" 
 
+def gen_markup1():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton("Data Science", callback_data="dsl"),InlineKeyboardButton("Machine Learning",callback_data="mll"),InlineKeyboardButton("Visualization",callback_data="vizl"))
+    return markup
+
+def gen_markup2():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton("Data Science", callback_data="dst"),InlineKeyboardButton("Machine Learning",callback_data="mlt"),InlineKeyboardButton("Visualization",callback_data="vizt"))
+    return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "dsl":
+        bot.send_message(heading,parse_mode='Markdown')
+        bot.send_message(new_linksds,parse_mode='HTML')
+    elif call.data == "dst":
+        bot.send_message(heading_ll,parse_mode='Markdown')
+        bot.send_message(lat_links,parse_mode='HTML')    
+
 @bot.message_handler(commands=['dev'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "I'm made by MaheshtheDev.\nHe is passionate about Data Science \nYou can Connect with my Developer:MaheshtheDev")
+    bot.send_message(message.chat.id, "I'm made by MaheshtheDev.\nHe is passionate about Data Science \nYou can Connect with my Developer!")
     bot.send_message(message.chat.id, sm_links,parse_mode='HTML')
 
 @bot.message_handler(commands=['trend'])
 def send_message(message):
-    bot.send_message(message.chat.id,heading,parse_mode='Markdown')
-    bot.send_message(message.chat.id,new_linksds,parse_mode='HTML')
+    bot.send_message(message.chat.id,"Select the Topic", reply_markup=gen_markup1())
+    # bot.send_message(message.chat.id,heading,parse_mode='Markdown')
+    # bot.send_message(message.chat.id,new_linksds,parse_mode='HTML')
 
 @bot.message_handler(commands=['latest'])
 def send_message(message):
-    bot.send_message(message.chat.id,heading_ll,parse_mode='Markdown')
-    bot.send_message(message.chat.id,lat_links,parse_mode='HTML')
+    bot.send_message(message.chat.id,"Select the Topic", reply_markup=gen_markup2())
+    # bot.send_message(message.chat.id,heading_ll,parse_mode='Markdown')
+    # bot.send_message(message.chat.id,lat_links,parse_mode='HTML')
 
 @server.route('/' + bot_token, methods=['POST'])
 def getMessage():
