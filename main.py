0fa7c32592111b9bbd9e567c98741b9a259d8eaf
MaheshtheDev <mahesh.svmr@gmail.com>
2020-05-17 21:53:59 +0530

    0.0.2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from flask import Flask, request
 from bs4 import BeautifulSoup
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-bot_token = '1193768093:AAFOCKx8uBDiyKJQjetH037-n7H4pMjehGY'
+bot_token = '1193768093:AAH9yTXq77fgpWHhv1HsusfBeunEo135fDc'
 bot = telebot.TeleBot(token=bot_token)
 server = Flask(__name__)
 
