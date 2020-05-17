abda7b66dfa5f6eba48d2ec9206b3e42478d1332
MaheshtheDev <mahesh.svmr@gmail.com>
2020-05-17 21:42:24 +0530

    Testing 01

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://salty-scrubland-38376.herokuapp.com/' + bot_token)
+    bot.set_webhook(url='https://warm-ridge-74785.herokuapp.com/' + bot_token)
     return "!", 200
 
 
